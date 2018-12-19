039c22e95e656dae649dee26e4931d802b1faf83
Uzziel Luna <uzziel.luna@ttu.edu>
2018-12-18 23:37:05 -0600

    Added list of Users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'TDIApp';
+
+  data: any[];
+
+  constructor (private dataService: DataService) {}
+
+  ngOnInit() {
+    this.dataService.getData().subscribe(data => {
+      this.data = data;
+    });
+  }
 }
